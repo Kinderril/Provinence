337774f8f2196e5559cb887d5e55ba933a4e6c57
Kinderril <kinderril@gmail.com>
2015-10-09 20:09:21 +0400

    items

--- Providence/Assets/Script/Shop/HeroShopRandomItem.cs
+++ Providence/Assets/Script/Shop/HeroShopRandomItem.cs
@@ -7,23 +7,56 @@ public class HeroShopRandomItem : IShopExecute
 {
     public override void Execute(int level)
     {
-        var slot = ShopController.Instance.RandomSlot();
+        var slot = ShopController.RandomSlot();
+        var levelResult = ShopController.RandomizeLvl(level);
+        Switcher(slot, levelResult);
+    }
+
+    private void Switcher(Slot slot, int levelResult)
+    {
+        var totalPoints = GetPointsByLvl(levelResult)*GetSlotCoef(slot);
+        float diff = UnityEngine.Random.Range(0.8f, 1f);
+        bool isRare = diff > 0.95f;
+        float contest = UnityEngine.Random.Range(0.65f, 1f);
+        if (contest > 0.9f)
+            contest = 1f;
+        var pparams = new Dictionary<ParamType,float>();
+        var primary = Connections.GetPrimaryParamType(slot);
+        var primaryValue = totalPoints*contest;
+        pparams.Add(primary,primaryValue);
+        if (contest < 0.95f)
+        {
+            var secondary = Connections.GetSecondaryParamType(slot);
+            var secondaryValue = totalPoints*(1f - contest);
+            pparams.Add(secondary, secondaryValue);
+        }
+        PlayerItem item = new PlayerItem(pparams,slot,isRare);
+    }
+
+    private int GetPointsByLvl(int lvl)
+    {
+        return lvl*4 + 20;
+    }
+
+    private float GetSlotCoef(Slot slot)
+    {
+        float val = 1f;
         switch (slot)
         {
             case Slot.physical_weapon:
+                val = 1.1f;
                 break;
             case Slot.magic_weapon:
+                val = 1.3f;
                 break;
             case Slot.body:
+                val = 1f;
                 break;
             case Slot.helm:
+                val = 0.8f;
                 break;
-            case Slot.bonus:
-                Execute(level);
-                return;
-
         }
-        //TODO creae random item
+        return val;
     }
 }
 
--- Providence/Assets/Script/Shop/ShopController.cs
+++ Providence/Assets/Script/Shop/ShopController.cs
@@ -6,22 +6,96 @@ using System.Text;
 
 public class ShopController : Singleton<ShopController>
 {
-    private List<Slot> allSlots = new List<Slot>(); 
     public void Init()
     {
-        foreach (Slot slot in Enum.GetValues(typeof(Slot)))
-        {
-            allSlots.Add(slot);
-        }
+        Connections.Init();
     }
-
-    public Slot RandomSlot()
-    {
-        return allSlots.RandomElement();
-    }
-
+    
     public void BuyItem(ShopItem shopItem)
     {
         shopItem.execute.Execute(shopItem.Parameter);
     }
+
+    public static int RandomizeLvl(int baseLvl)
+    {
+        var lvls = new WDictionary<int>(new Dictionary<int, float>()
+        {
+            { baseLvl -1,3f },
+            { baseLvl ,4f },
+            { baseLvl +1,3f },
+            { baseLvl +2,0.5f },
+        });
+        return lvls.Random();
+    }
+
+    public static Slot RandomSlot()
+    {
+        var slots = new WDictionary<Slot>(new Dictionary<Slot, float>()
+        {
+            { Slot.body,3f },
+            { Slot.bonus,3f },
+            { Slot.helm,3f },
+            { Slot.magic_weapon,3f },
+            { Slot.physical_weapon,3f },
+        });
+        return slots.Random();
+    }
+
+    
 }
+
+public static class Connections
+{
+    private static readonly Dictionary<Slot, WDictionary<ParamType>> primary = new Dictionary<Slot, WDictionary<ParamType>>();
+    private static readonly Dictionary<Slot, WDictionary<ParamType>> secondary = new Dictionary<Slot, WDictionary<ParamType>>();
+    public static void Init()
+    {
+        primary.Add(Slot.physical_weapon, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.PPower, 1 } }));
+        primary.Add(Slot.magic_weapon, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.MPower, 3 } }));
+        primary.Add(Slot.body, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.PDef, 3 }, { ParamType.MDef, 1 } }));
+        primary.Add(Slot.helm, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.PDef, 1 }, { ParamType.MDef, 3 } }));
+
+        secondary.Add(Slot.physical_weapon, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.PDef, 1 } }));
+        secondary.Add(Slot.magic_weapon, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.MDef, 3 } }));
+        secondary.Add(Slot.body, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.Hp, 1 }, { ParamType.Speed, 1 } }));
+        secondary.Add(Slot.helm, new WDictionary<ParamType>(new Dictionary<ParamType, float>() { { ParamType.Hp, 1 }, { ParamType.PPower, 3 }, { ParamType.MPower, 3 } }));
+    }
+
+    public static ParamType GetPrimaryParamType(Slot slot)
+    {
+        return primary[slot].Random();
+    }
+    public static ParamType GetSecondaryParamType(Slot slot)
+    {
+        return secondary[slot].Random();
+    }
+}
+
+public class WDictionary<T> : Dictionary<float, T>
+{
+    private float total = 0;
+    public WDictionary(Dictionary<T,float> items )
+    {
+        float curW = 0;
+        foreach (var item in items)
+        {
+            curW += item.Value;
+            Add(curW,item.Key);
+        }
+        total = curW;
+    }
+
+    public T Random()
+    {
+        var res = UnityEngine.Random.Range(0, total);
+        foreach (var key in Keys)
+        {
+            if (key >= res)
+            {
+                return this[key];
+            }
+        }
+        return default(T);
+    }
+}
+
