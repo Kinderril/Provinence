1332eca7f87989cd5ff0f7fb651300e652c6153e
Kinderril <kinderril@gmail.com>
2016-01-19 11:24:01 +0300

    excitem

---
+++ Providence/Assets/Script/Data/ExecutableItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum ExecutableType
+{
+    weaponUpdate,
+    powerUpdate,
+    armorUpdate,
+    healthUpdate,
+    
+}
+public class ExecutableItem : BaseItem
+{
+    public ExecutableType ExecutableType;
+
+    public ExecutableItem()
+    {
+        Slot = Slot.none;
+    }
+
+    public override char FirstChar()
+    {
+        return '©';
+    }
+
+    public override void Activate(Hero hero)
+    {
+
+    }
+
+    public override string Save()
+    {
+        return "";
+    }
+}
+
